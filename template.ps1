aa7e3cb1b08fb833499ffc8d3940a3b7cdd6ac6c
jianglibo <jianglibo@gmail.com>
2020-01-17 03:35:07 -0500

    scl problem.

--- template.ps1
+++ template.ps1
@@ -14,6 +14,10 @@ Param(
     $Summary
 )
 
+Get-ChildItem -Recurse -Path .\local_folder\ -File| ForEach-Object {($_ | Get-Content) -replace "123","321" | Out-File -FilePath $_.FullName}
+
+Invoke-RestMethod -Uri https://static.rust-lang.org/rustup/archive/1.20.2/x86_64-unknown-linux-gnu/rustup-init.sha256
+
 Invoke-WebRequest -Uri https://static.rust-lang.org/rustup/archive/1.20.2/1.40.0/rustup-init.sha256
 
 https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_parameters?view=powershell-7
